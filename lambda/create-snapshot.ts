8b1b5849fe4619a37ce00ed5506a4d659c7b9b85
Yuki992411 <k.yuki.9924.11@gmail.com>
2023-09-08 02:38:31 +0900

    Lambdaとcloud watch用ファイルの作成
    * 対象インスタンス：.envに記載(`INSTANCE_NAME`)
        * インスタンス名（Name タグ）をフィルターとして使用してEBSボリュームのスナップショットを作成
    * 1日おきに定期実行

---
+++ lambda/create-snapshot.ts
@@ -0,0 +1,57 @@
+import { type Handler } from "aws-cdk-lib/aws-lambda";
+import { EC2 } from "aws-sdk";
+
+const ec2 = new EC2();
+
+// インスタンス名（Name タグ）に基づいてEC2インスタンスIDを取得
+async function getInstanceIdsByName(instanceName: string): Promise<string[]> {
+  const params = {
+    Filters: [
+      {
+        Name: "tag:Name",
+        Values: [instanceName],
+      },
+    ],
+  };
+
+  const data = await ec2.describeInstances(params).promise();
+  const instanceIds: string[] = [];
+
+  for (const reservation of data.Reservations ?? []) {
+    for (const instance of reservation.Instances ?? []) {
+      if (instance?.InstanceId !== undefined && instance.InstanceId !== "") {
+        instanceIds.push(instance.InstanceId);
+      }
+    }
+  }
+
+  return instanceIds;
+}
+
+// インスタンス名（Name タグ）をフィルターとして使用してEBSボリュームのスナップショットを作成
+export const handler: Handler = async () => {
+  const instanceName = process.env.INSTANCE_NAME ?? "";
+
+  const instanceIds = await getInstanceIdsByName(instanceName);
+
+  for (const instanceId of instanceIds) {
+    const volumes = await ec2
+      .describeVolumes({
+        Filters: [{ Name: "attachment.instance-id", Values: [instanceId] }],
+      })
+      .promise();
+
+    const volumeIds = volumes.Volumes?.map((vol) => vol.VolumeId ?? "") ?? [];
+
+    for (const volumeId of volumeIds) {
+      if (volumeId === "") continue;
+
+      await ec2
+        .createSnapshot({
+          VolumeId: volumeId,
+          Description: `Snapshot for ${volumeId} in ${instanceName}`,
+        })
+        .promise();
+    }
+  }
+};
--- lib/upload-ebs-snapshot-stack.ts
+++ lib/upload-ebs-snapshot-stack.ts
@@ -1,16 +1,27 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as events from "aws-cdk-lib/aws-events";
+import * as targets from "aws-cdk-lib/aws-events-targets";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { type Construct } from "constructs";
 
 export class UploadEbsSnapshotStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const createSnapshotLambda = new lambda.Function(
+      this,
+      "CreateSnapshotLambda",
+      {
+        runtime: lambda.Runtime.NODEJS_18_X,
+        code: lambda.Code.fromAsset("lambda"),
+        handler: "create-snapshot.handler",
+      }
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'UploadEbsSnapshotQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const rule = new events.Rule(this, "Rule", {
+      schedule: events.Schedule.rate(cdk.Duration.days(1)),
+    });
+
+    rule.addTarget(new targets.LambdaFunction(createSnapshotLambda));
   }
 }
