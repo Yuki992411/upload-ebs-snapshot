79b1fcb3f327ad19db0be05c97cbc913e2bacc92
Yuki992411 <k.yuki.9924.11@gmail.com>
2023-09-10 17:59:51 +0900

    lambdaファイルの各メソッドを実行するのに必要なポリシーを作成

--- lib/upload-ebs-snapshot-stack.ts
+++ lib/upload-ebs-snapshot-stack.ts
@@ -1,6 +1,7 @@
 import { Duration, Stack, type StackProps } from "aws-cdk-lib";
 import { Rule, Schedule } from "aws-cdk-lib/aws-events";
 import { LambdaFunction } from "aws-cdk-lib/aws-events-targets";
+import { Effect, Policy, PolicyStatement } from "aws-cdk-lib/aws-iam";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { type Construct } from "constructs";
@@ -14,6 +15,24 @@ export class UploadEbsSnapshotStack extends Stack {
       "create-snapshot-lambda",
       { runtime: Runtime.NODEJS_18_X }
     );
+
+    const customPolicy = new Policy(this, "CustomPolicy", {
+      statements: [
+        new PolicyStatement({
+          actions: [
+            "ec2:DescribeInstances",
+            "ec2:DescribeVolumes",
+            "ec2:CreateSnapshot",
+          ],
+          effect: Effect.ALLOW,
+          resources: ["*"],
+        }),
+      ],
+    });
+
+    if (createSnapshotLambda.role != null) {
+      customPolicy.attachToRole(createSnapshotLambda.role);
+    }
 
     const rule = new Rule(this, "Rule", {
       schedule: Schedule.rate(Duration.days(1)),
